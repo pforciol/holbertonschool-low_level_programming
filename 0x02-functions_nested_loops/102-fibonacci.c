45105c592868363f47535d0740644b177fd84037
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2020-09-22 08:55:53 +0000

    Add advanced task 102 of 0x02 - C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 fibonacci numbers
+ *
+ * Return: always 0 (Success)
+ */
+
+int	main(void)
+{
+	unsigned long n_minus1 = 1;
+	unsigned long n_minus2 = 0;
+	unsigned long n = 0;
+	int i = 0;
+
+	printf("0, 1");
+	while (i < 48)
+	{
+		printf(", ");
+		n = n_minus1 + n_minus2;
+		n_minus2 = n_minus1;
+		n_minus1 = n;
+		printf("%lu", n);
+		i++;
+	}
+	return (0);
+}
