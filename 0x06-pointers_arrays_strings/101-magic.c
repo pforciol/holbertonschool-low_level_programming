67e8a103ebb50560ac6754a703d006bc99ac2cdb
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2020-10-01 12:53:11 +0000

    Fix betty style on 101-magic.x of 0x06

--- 0x06-pointers_arrays_strings/101-magic.c
+++ 0x06-pointers_arrays_strings/101-magic.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - entry point
+ * Return: 0
+ */
+
 int main(void)
 {
 	int n;
@@ -16,7 +21,7 @@ int main(void)
 	 * - only one statement
 	 * - you are not allowed to code anything else than this line of code
 	 */
-	*(p + 5) = 98;
+	*(n + 5) = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
