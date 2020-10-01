e7df8736c3117ceb74bf0122441b96460b92fa8d
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2020-10-01 12:38:00 +0000

    Add optional tasks 100 and 101 of 0x06

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * _abs - returns absolute value of given int
+ * @n: number for which we want to get the absolute value
+ *
+ * Return: absolute value of number n
+ */
+
+int		_abs(int n)
+{
+	return ((n < 0) ? -n : n);
+}
+
+/**
+ * print_number - prints number on the standard output
+ * @n: the number to be printed
+ */
+
+void	print_number(int n)
+{
+	if (n < 0)
+		_putchar('-');
+
+	if (n >= 10 || n <= -10)
+		print_number(_abs(n / 10));
+	_putchar(_abs(n % 10) + '0');
+}
