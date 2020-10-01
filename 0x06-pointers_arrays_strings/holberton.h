b23ea2ea77c915e08e1a04493325f0af236f8ebf
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2020-10-01 12:39:04 +0000

    Add holberton.h for optional tasks

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -9,7 +9,8 @@ int		_strcmp(char *, char *);
 void	reverse_array(int *, int);
 char	*string_toupper(char *);
 char	*cap_string(char *);
-char	*leet(char *);
+char	*leet(char *s);
 char	*rot13(char *);
+void	print_number(int);
 
 #endif
