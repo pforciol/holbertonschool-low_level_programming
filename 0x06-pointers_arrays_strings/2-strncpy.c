348fd81f8bc2d6b649ee2c29b14101e0807c18b1
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2020-10-01 12:06:11 +0000

    Fix tasks 2 and 3 of 0x05

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,10 +11,12 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while (i < n)
+	while (i < n && src[i])
 	{
 		dest[i] = src[i];
 		i++;
 	}
+	while (i < n)
+		dest[i++] = '\0';
 	return (dest);
 }
