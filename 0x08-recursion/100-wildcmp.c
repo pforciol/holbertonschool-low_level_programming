dd4ff55b879aa265de01ec244be472d61c33a84d
Pierre Forcioli <2500@holbeetonschool.com>
2020-10-07 10:26:09 +0000

    Fix 100

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -12,12 +12,15 @@ int		compare(char *s1, char *s2)
 {
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-	if (*s1 == '\0' && *s2 == '*')
-		return (compare(s1, s2 + 1));
 	if (*s1 == *s2)
 		return (compare(s1 + 1, s2 + 1));
 	if (*s2 == '*')
-		return (compare(s1, s2 + 1) || compare(s1 + 1, s2));
+	{
+		if (*s1 == '\0')
+			return (compare(s1, s2 + 1));
+		else
+			return (compare(s1, s2 + 1) || compare(s1 + 1, s2));
+	}
 	return (0);
 }
 
