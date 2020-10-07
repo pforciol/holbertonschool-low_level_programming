359bb4b175731736b5754400caa97cc6c62806eb
Pierre Forcioli <2500@holbeetonschool.com>
2020-10-07 10:19:40 +0000

    Fix task 100

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -12,6 +12,8 @@ int		compare(char *s1, char *s2)
 {
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
+	if (*s1 == '\0' && *s2 == '*')
+		return (compare(s1, s2 + 1));
 	if (*s1 == *s2)
 		return (compare(s1 + 1, s2 + 1));
 	if (*s2 == '*')
