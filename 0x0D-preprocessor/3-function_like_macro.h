116f27872cffae9f1ced797ea5573a23d98be56d
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2020-10-19 09:13:54 +0000

    Fix betty on 3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef FUNCTION_LIKE_MACRO
-#define FUNCTION_LIKE_MARCO
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 
 #define ABS(x) ((x) < (0) ? (-x) : (x))
 
-#endif /* FUNCTION_LIKE_MACRO */
+#endif /* FUNCTION_LIKE_MACRO_H */
