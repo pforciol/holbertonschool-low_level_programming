f5c4bdbc28d8cfe5ad97fa3cf815e6d8de3dd7c5
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2020-10-24 14:01:27 +0000

    Fix asm and header file

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -12,5 +12,5 @@ main:
 	int	0x80
 
 section .data
-msg db 'Hello, Holberton', 0xa
-len	equ	$ - msg
+	msg db 'Hello, Holberton',0xa
+	len	equ	$ - msg
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -17,12 +17,17 @@
 typedef struct specs
 {
 	char *spec;
-	char *pf_spec;
+	void (*func)(va_list);
 } specs_t;
 
 int		sum_them_all(const unsigned int n, ...);
 void	print_numbers(const char *separator, const unsigned int n, ...);
 void	print_strings(const char *separator, const unsigned int n, ...);
 void	print_all(const char * const format, ...);
+
+void	print_char(va_list list);
+void	print_int(va_list list);
+void	print_float(va_list list);
+void	print_string(va_list list);
 
 #endif /* VARIADIC_FUNCTIONS_H */
