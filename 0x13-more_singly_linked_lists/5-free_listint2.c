9aa47db233627f6648ede924bdbeeeb6bcd20b37
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2020-11-03 14:18:15 +0000

    Fix task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,11 +10,14 @@ void	free_listint2(listint_t **head)
 {
 	listint_t *node = NULL;
 
-	while (head && *head)
+	if (head && *head)
 	{
-		node = *head;
-		*head = (*head)->next;
-		free(node);
+		while (head && *head)
+		{
+			node = *head;
+			*head = (*head)->next;
+			free(node);
+		}
+		*head = NULL;
 	}
-	*head = NULL;
 }
