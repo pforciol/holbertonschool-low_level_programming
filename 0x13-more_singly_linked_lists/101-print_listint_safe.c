e61619bff08407506fd5989377e92703f6538959
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2020-11-03 16:20:13 +0000

    Fix 101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,6 +1,29 @@
 #include "lists.h"
 
 /**
+ * interval - checks if the linked list has not looped
+ *
+ * @begin: the begining of the list
+ * @node: the current node
+ * @i: the interval that should be at current position
+ *
+ * Return: 1 if interval is correct, 0 is there is a loop
+ */
+
+size_t	interval(const listint_t *begin, const listint_t *node, size_t i)
+{
+	size_t check = 0;
+
+	while (begin != node)
+	{
+		begin = begin->next;
+		check++;
+	}
+
+	return ((check == i) ? 1 : 0);
+}
+
+/**
  * print_listint_safe - prints a listint_t linked list
  *
  * @head: the listint_t argument (head)
@@ -11,21 +34,20 @@
 size_t	print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
-	const listint_t *curr = head, *tmp;
+	const listint_t *begin = head;
 
-	while (curr)
+	if (head)
 	{
-		printf("[%p] %d\n", (void *)curr, curr->n);
-
-		tmp = curr;
-		curr = curr->next;
-
-		count++;
-		if (curr > tmp)
+		while (head && interval(begin, head, count))
 		{
-			printf("-> [%p] %d\n", (void *)curr, curr->n);
-			break;
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+
+			count++;
 		}
+
+		if (head)
+			printf("-> [%p] %d\n", (void *)head, head->n);
 	}
 	return (count);
 }
