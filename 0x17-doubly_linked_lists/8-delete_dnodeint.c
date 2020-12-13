925b6e2a2b55819fd0bea20e4cd2d279c3c45367
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2020-12-13 20:32:13 +0000

    Add last tasks

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int i = 0;
+	dlistint_t *tmp;
+	dlistint_t *to_del = *head;
+
+	if (*head && head)
+	{
+		if (index == 0)
+		{
+			tmp = *head;
+			*head = (*head)->next;
+			if (*head)
+				(*head)->prev = NULL;
+			free(tmp);
+		}
+		else
+		{
+			while (i != index && to_del)
+			{
+				tmp = to_del;
+				to_del = tmp->next;
+				i++;
+			}
+			if (i == index && to_del)
+			{
+				tmp->next = to_del->next;
+				if (to_del->next)
+					to_del->next->prev = tmp;
+				free(to_del);
+			}
+			else
+				return (-1);
+		}
+		return (1);
+	}
+	return (-1);
+}
