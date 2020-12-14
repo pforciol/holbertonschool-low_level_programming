98aff2d5803e61c80143453ab45434443d00c18d
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2020-12-14 16:22:09 +0000

    Fix libmmw.so

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp -q "https://github.com/pforciol/holbertonschool-low_level_programming/tree/master/0x18-dynamic_libraries/libmmw.so"
-LD_PRELOAD=/tmp/libmmw.so
+wget -P /tmp -q "https://github.com/pforciol/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/libmmw.so"
+export LD_PRELOAD="/tmp/libmmw.so"
