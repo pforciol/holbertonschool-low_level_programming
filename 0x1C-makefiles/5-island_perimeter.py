3f3a9080dca845fba10249ede177344b71faa455
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2021-02-19 11:06:51 +0000

    Add all tasks of the Makefile day

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -13,17 +13,17 @@ def neighbour(grid, y, x):
             y (int): height parameter.
             x (int): width parameter.
     """
-    nb = 0
+    nb = 4
 
     if (y > 0) and grid[y - 1][x]:
-        nb += 1
+        nb -= 1
     if (x > 0) and grid[y][x - 1]:
-        nb += 1
+        nb -= 1
     if (y < len(grid) - 1) and grid[y + 1][x]:
-        nb += 1
+        nb -= 1
     if (x < len(grid[0]) - 1) and grid[y][x + 1]:
-        nb += 1
-    return 4 - nb
+        nb -= 1
+    return nb
 
 
 def island_perimeter(grid):
