e0aecc8edb81e35e5478aade092bbbd242acd81f
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2021-04-21 19:55:13 +0000

    Add last two exercices and fix betty style on header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -49,10 +49,10 @@ listint_t *jump_list(listint_t *list, size_t size, int value);
 
 typedef struct skiplist_s
 {
-    int n;
-    size_t index;
-    struct skiplist_s *next;
-    struct skiplist_s *express;
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
 } skiplist_t;
 
 skiplist_t *linear_skip(skiplist_t *list, int value);
