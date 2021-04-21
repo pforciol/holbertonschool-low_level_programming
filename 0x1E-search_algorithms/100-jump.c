36f270d15578374cd99056b874a466b49fcadc6e
Pierre Forcioli <pierre.forcioli@holbertonschool.com>
2021-04-21 13:18:05 +0000

    Try another fix

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -16,7 +16,7 @@ int jump_search(int *array, size_t size, int value)
 
 	if (array && size > 0)
 	{
-		while (i <= size)
+		while (i < size)
 		{
 			printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 
